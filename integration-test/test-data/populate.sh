4eb8ad4865b66ad115e9fd18f1405ff045be9bdf
Ilya Kreymer <ikreymer@gmail.com>
2018-02-02 12:58:16 -0800

    integration-tests: - add pywb image from ukwa/pywb to compose, add test config.yaml (#4) - add hadoop image to override 'safe-mode extension' setting after name node commit to allow for faster test startup

--- integration-test/test-data/populate.sh
+++ integration-test/test-data/populate.sh
@@ -8,7 +8,7 @@ echo INFO - send CDX data:
 curl -s -XPOST --data-binary @acid.matkelly.com-20140716200357.cdx http://cdx:8080/tc
 
 echo INFO - wait for HDFS to be ready...
-sleep 30
+sleep 2
 echo INFO - uploading file:
 curl -L -i -X PUT -T acid.matkelly.com-20140716200357.warc.gz "http://hadoop:50075/webhdfs/v1/acid.matkelly.com-20140716200357.warc.gz?&op=CREATE&createparent=false&overwrite=false&namenoderpcaddress=hadoop:9000&user.name=root"
 
