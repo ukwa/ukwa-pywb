1cf53c8f2b588da3ba13e4270e85486469fc5b24
Ilya Kreymer <ikreymer@gmail.com>
2018-02-02 16:48:27 -0800

    integration-test: add more complete acid test capture to test suite

--- integration-test/test-data/populate.sh
+++ integration-test/test-data/populate.sh
@@ -6,10 +6,14 @@ echo INFO - wait for CDX server to be ready...
 sleep 10
 echo INFO - send CDX data:
 curl -s -XPOST --data-binary @acid.matkelly.com-20140716200357.cdx http://cdx:8080/tc
+curl -s -XPOST --data-binary @acidtest-20180203004221.cdx http://cdx:8080/tc
 
 echo INFO - wait for HDFS to be ready...
 sleep 2
 echo INFO - uploading file:
 curl -L -i -X PUT -T acid.matkelly.com-20140716200357.warc.gz "http://hadoop:50075/webhdfs/v1/acid.matkelly.com-20140716200357.warc.gz?&op=CREATE&createparent=false&overwrite=false&namenoderpcaddress=hadoop:9000&user.name=root"
+
+curl -L -i -X PUT -T acidtest-20180203004221.warc.gz "http://hadoop:50075/webhdfs/v1/acidtest-20180203004221.warc.gz?&op=CREATE&createparent=false&overwrite=false&namenoderpcaddress=hadoop:9000&user.name=root"
+
 
 echo INFO - all done.
