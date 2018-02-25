f799d0f405af0358c531438e29eb5bb3a0e102eb
Ilya Kreymer <ikreymer@gmail.com>
2018-02-24 18:20:20 -0800

    run script: fix typo in run script

--- run-ukwa-pywb.sh
+++ run-ukwa-pywb.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # local paths to cdx and warc data
-UKWA_INDEX=./integration-test/test-data/
-UKWA_ARCHIVE=./integration-test/test-data/
+UKWA_INDEX=./integration-test/test-data/ \
+UKWA_ARCHIVE=./integration-test/test-data/ \
 python ukwa_pywb/ukwa_app.py $@
 
 
